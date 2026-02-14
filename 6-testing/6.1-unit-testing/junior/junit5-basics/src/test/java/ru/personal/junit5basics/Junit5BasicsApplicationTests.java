525a0d5520ba81c0c0a2bcaba6a8b0e9fd30db52
Jimbanan <uservice371@mail.ru>
2026-02-14 09:54:38 +0300

    init commit

---
+++ 6-testing/6.1-unit-testing/junior/junit5-basics/src/test/java/ru/personal/junit5basics/Junit5BasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.personal.junit5basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5BasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
